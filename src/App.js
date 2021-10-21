29c153b60efd833749a8bae36c03f6e9dfc563a7
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-21 12:17:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
+import SignInSide from './components/SignInSide'
+import { Home } from './components/Home';
+import Dashboard from './components/DashBoard/Dashboard'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<SignInSide />
+				</Route>
+				<Route exact path="/home">
+					<Dashboard />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import './home.css'
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <Card variant="outlined">
+                <Button variant="contained">ddsfhdsfds</Button>
+            </Card>
+
+        </div>
+    )
+}
