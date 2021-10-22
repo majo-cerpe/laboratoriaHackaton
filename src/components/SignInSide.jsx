87ae9cf98caf5f31001083bc2f0ae82f7ddcdb82
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-22 12:45:48 -0300

    LockOutlinedIcon implemented

--- src/components/SignInSide.jsx
+++ src/components/SignInSide.jsx
@@ -9,7 +9,7 @@ import Checkbox from '@mui/material/Checkbox';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-/* import LockOutlinedIcon from '@mui/icons-material/LockOutlined'; */
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
@@ -94,7 +94,7 @@ export default function SignInSide() {
                         }}
                     >
                         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            {/* <LockOutlinedIcon /> */}
+                            <LockOutlinedIcon />
                         </Avatar>
                         <Typography component="h1" variant="h5">
                             Sign in
