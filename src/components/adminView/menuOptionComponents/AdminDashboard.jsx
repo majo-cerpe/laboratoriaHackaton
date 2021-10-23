fa561b7a65c9b066dfa8e836c925355c56bf1ed7
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-23 19:49:11 -0300

    temporary description for dashboard component

--- src/components/adminView/menuOptionComponents/AdminDashboard.jsx
+++ src/components/adminView/menuOptionComponents/AdminDashboard.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const AdminDashboard = () => {
     return (
         <div>
-            
+            <h3>DashBoard</h3>
+            <p>acá van las estadísticas del sitio y otros datos relevantes para la administración del mismo</p>
         </div>
     )
 }
