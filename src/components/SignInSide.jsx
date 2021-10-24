c8fd688cd40a4f936bcf113a909c670d881d08c3
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-24 01:40:03 -0300

    unused imports deleted and copyright href fixed

--- src/components/SignInSide.jsx
+++ src/components/SignInSide.jsx
@@ -5,14 +5,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-/* import Link from '@mui/material/Link'; */
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import firabase from '../firebase'
 
@@ -21,7 +19,7 @@ function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
-            <a color="inherit" href="https://mui.com/">
+            <a color="inherit" href="https://stefanini.com/es">
                 Stefanini
             </a>{' '}
             {new Date().getFullYear()}
