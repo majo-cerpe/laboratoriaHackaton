4c78e2178e0d6f8907a9ad3abcc91d1bb9f3878e
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-25 18:26:58 -0300

    minor changes

---
+++ src/components/adminView/menuOptionComponents/MomentsAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Moments = () => {
+    return (
+        <div>
+            Administración de Momentos Stefeniners       
+        </div>
+    )
+}
+
+export default Moments;
