8ed09a5db31ea182ea10eb18e781a51731545ac8
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-25 00:19:21 -0300

    table format fixed and new fields added

--- src/components/adminView/menuOptionComponents/userAdmin/UsersTable.jsx
+++ src/components/adminView/menuOptionComponents/userAdmin/UsersTable.jsx
@@ -3,13 +3,14 @@ import { DataGrid } from '@mui/x-data-grid';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
+import Divider from '@mui/material/Divider';
 import './userAdmin.css'
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'firstName', headerName: 'Nombres', width: 140 },
     { field: 'lastName', headerName: 'Apellidos', width: 140 },
-    { field: 'dateOfBirth', headerName: 'Fecha de nacimiento', type: 'date', width: 140 },
+    { field: 'dateOfBirth', headerName: 'Fecha de nacimiento', width: 220 },
     /*  {
          field: 'fullName',
          headerName: 'Full name',
@@ -20,22 +21,25 @@ const columns = [
              `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''
              }`,
      }, */
-    { field: 'pronouns', headerName: 'Pronombres', width: 140 },
-    { field: 'tshirtSize', headerName: 'Talla de polera', width: 140 },
-    { field: 'shoeSize', headerName: 'Talla de calzado', width: 140 },
-    { field: 'numberOfChildren', headerName: 'Cantidad de hijos', width: 140 }
+    { field: 'pronouns', headerName: 'Pronombres', width: 160 },
+    { field: 'tshirtSize', headerName: 'Talla polera', width: 150 },
+    { field: 'shoeSize', headerName: 'Talla calzado', width: 160 },
+    { field: 'numberOfChildren', headerName: 'Hijos', width: 140 }
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 2, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 3, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 4, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 5, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 6, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 7, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 8, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 9, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 10, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 11, lastName: 'Juan', firstName: 'Perez', dateOfBirth: '3-12-1986', pronouns: 'Él', tshirtSize: 'L', shoeSize: '42', numberOfChildren: 'no tiene' },
+    { id: 12, lastName: null, firstName: null, dateOfBirth: null, pronouns: null, tshirtSize: null, shoeSize: null, numberOfChildren: null },
 ];
 
 const UsersTable = () => {
@@ -46,55 +50,47 @@ const UsersTable = () => {
                 sx={{
                     p: 2,
                     display: 'flex',
-                    borderRadius: 2,
-                    margin: 3,
-                }}
-                elevation={12}
-            >
-                <Stack direction="row" spacing={30}>
-                    <Stack direction="row" spacing={2}>
-                        <TextField 
-                        id="outlined-size-small" 
-                        size="small" 
-                        label="Ingrese nombre" 
-                        variant="outlined" 
-                        />
-                        <button variant="contained" className="userAdminbutton">Buscar por nombre</button>
-                    </Stack>
-                    <Stack direction="row" spacing={2}>
-                        <TextField 
-                        id="outlined-size-small" 
-                        size="small" 
-                        label="Ingrese apellido" 
-                        variant="outlined" 
-                        />
-                        <button variant="contained" className="userAdminbutton">Buscar por apellido</button>
-                    </Stack>
-                </Stack>
-            </Paper>
-            <Paper
-                sx={{
-                    p: 2,
-                    display: 'flex',
                     flexDirection: 'column',
-                    height: 420,
+                    height: 650,
                     borderRadius: 2,
                     margin: 3
                 }}
                 elevation={12}
             >
+                <Stack direction="row"  divider={<Divider orientation="vertical" flexItem />} spacing={{ xs: 4, sm: 8, md: 16 }} marginBottom={3}>
+                    <Stack direction="row" spacing={2}>
+                        <TextField
+                            id="outlined-size-small"
+                            size="small"
+                            label="Ingrese nombre"
+                            variant="outlined"
+                        />
+                        <button variant="contained" className="userAdminbutton">Buscar por nombre</button>
+                    </Stack>
+                    <Stack direction="row" spacing={2}>
+                        <TextField
+                            id="outlined-size-small"
+                            size="small"
+                            label="Ingrese apellido"
+                            variant="outlined"
+                        />
+                        <button variant="contained" className="userAdminbutton">Buscar por apellido</button>
+                    </Stack>
+                </Stack>
                 <DataGrid
                     rows={rows}
                     columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
                     checkboxSelection
+                    rowHeight={35}
                 />
+                <Stack direction="row" spacing={2} justifyContent="center" paddingTop='15px'>
+                    <button variant="contained" className="userAdminbutton">Borrar usuario</button>
+                    <button variant="contained" className="userAdminbutton">Editar usuario</button>
+                </Stack>
             </Paper>
-            <Stack direction="row" spacing={2} justifyContent="center">
-                <button variant="contained" className="userAdminbutton">Borrar usuario</button>
-                <button variant="contained" className="userAdminbutton">Editar usuario</button>
-            </Stack>
+
         </div>
     );
 }
