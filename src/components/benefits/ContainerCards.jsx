2de86b37e81e70f379475ce65e7c1e6e8481cdc8
ViviMoya <v.moyacapitan@gmail.com>
2021-10-25 16:39:09 -0300

    db

--- src/components/benefits/ContainerCards.jsx
+++ src/components/benefits/ContainerCards.jsx
@@ -1,30 +1,52 @@
 import { Grid } from '@mui/material'
 import React, { Fragment } from 'react'
-import benefits from '../benefits/benefits.json'
 import CardBenefits from './CardBenefits'
+
+
+import { collection, getDocs } from "firebase/firestore";
+import app from './../../firebase'
+import db from './../../firebase'
+
 
 
 
 
 const ContainerCards = () => {
 
+const [task, setTask] = React.useState([])
 
-	return (
-   <Fragment> 
-	 
-    <h1>BENEFICIOS CORPORATIVOS</h1>
-    <Grid container spacing={1} justify="center"> 
-    {benefits.map(((item) => {
-    	return <CardBenefits
-    	 title={item.title}
-    	 description={item.description}
-    	 image={item.image}
-    	 text={item.text}
-    	 secondaryText={item.secondaryText}
-	  />
-    }))}
-   </Grid>
-   </Fragment>
-	)
+React.useEffect(() => {
+
+const getData =  async () => {
+	try {
+
+	const data = await getDocs(collection(app, "benefits"));
+	data.forEach((doc) => {
+	  console.log(`${doc.id} => ${doc.data()}`);
+	});
+
+}catch (error) {
+		console.log(error);
+	}
+}
+getData()
+}, [])
+
+		return (
+			<Fragment> 
+			 <h1>BENEFICIOS CORPORATIVOS</h1>
+			 <Grid container spacing={1} justify="center"> 
+			 {task.map(((item) => {
+				 return <CardBenefits
+				  title={item.title}
+				  description={item.description}
+				  image={item.image}
+				  text={item.text}
+				  secondaryText={item.secondaryText}
+			   />
+			 }))}
+			</Grid>
+			</Fragment>
+			 )
 }
 export default ContainerCards
