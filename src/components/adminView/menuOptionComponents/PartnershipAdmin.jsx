652cbf9257ef636119ba5790dab4305e21e70ae4
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-26 16:07:15 -0300

    admin menu options with the same design

--- src/components/adminView/menuOptionComponents/PartnershipAdmin.jsx
+++ src/components/adminView/menuOptionComponents/PartnershipAdmin.jsx
@@ -4,59 +4,59 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import './menuOptionsComponents.css'
 
 const PartnerAdmin = () => {
-    const [title, setTitle] = React.useState('');
-    const [description, setDescription] = React.useState('');
-    const [text, setText] = React.useState('');
-    const [secondaryText, setSecondaryText] = React.useState('');
+	const [title, setTitle] = React.useState('');
+	const [description, setDescription] = React.useState('');
+	const [text, setText] = React.useState('');
+	const [secondaryText, setSecondaryText] = React.useState('');
 
-   
-    return (
-        <Fragment> 
-	 
-    <h4>Administración de convenios</h4>
-    <Container maxWidth="sm">
-    <Grid container spacing={1} justify="center"> 
-  
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      
-      <div>
-        <TextField
-          id="outlined-multiline-flexible"
-          label="INGRESA NUEVO CONVENIO"
-          multiline
-         rows={4}
-         value={title}
-    onChange={e => setTitle(e.target.value)}
-          variant="outlined"
-        />
 
-        <TextField
-          id="outlined-multiline-static"
-          label="INGRESA LA DESCRIPCIÓN"
-          multiline
-          rows={4}
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-          variant="outlined"
-        />
-         <Button  type="submit" variant="contained">INGRESAR CONVENIO</Button>
-      </div>
-     
-     
-    </Box>
-   </Grid>
-   </Container>
-   </Fragment>
-    )
+	return (
+		<Fragment>
+			<p className="optionHeader">Administración de convenios</p>
+			<Container maxWidth="sm">
+				<Grid container spacing={1} justify="center">
+
+					<Box
+						component="form"
+						sx={{
+							'& .MuiTextField-root': { m: 1, width: '25ch' },
+						}}
+						noValidate
+						autoComplete="off"
+					>
+
+						<div>
+							<TextField
+								id="outlined-multiline-flexible"
+								label="INGRESA NUEVO CONVENIO"
+								multiline
+								rows={4}
+								value={title}
+								onChange={e => setTitle(e.target.value)}
+								variant="outlined"
+							/>
+
+							<TextField
+								id="outlined-multiline-static"
+								label="INGRESA LA DESCRIPCIÓN"
+								multiline
+								rows={4}
+								value={description}
+								onChange={e => setDescription(e.target.value)}
+								variant="outlined"
+							/>
+							<button className="menuOptionsbutton" type="submit" variant="contained">Ingresar convenio</button>
+						</div>
+
+
+					</Box>
+				</Grid>
+			</Container>
+		</Fragment>
+	)
 }
 
 export default PartnerAdmin;
