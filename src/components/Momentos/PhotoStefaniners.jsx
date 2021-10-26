a506c25e45e197223b99bba720eb896feb21fdca
Paula Ramirez <paularamirezsot@gmai.com>
2021-10-26 17:43:16 -0300

    fotos stefaniners

--- src/components/Momentos/PhotoStefaniners.jsx
+++ src/components/Momentos/PhotoStefaniners.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import image from './momentos.json'; 
+
+
 
 const Photos = () => {
     return (
-        <div>
-           <h1> Momentos Stefaniners</h1>
-           <p>Nuestros mejores momentos</p>
-        </div>
+<Box sx={{ width: 600, height: 550,  }}>
+      <ImageList variant="masonry" cols={2} gap={15}>
+        {image.map((post) => (
+          <ImageListItem key={post.image}>
+            <img
+              src={`${post.image}?w=248&fit=crop&auto=format`}
+              srcSet={`${post.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={post.title}
+              
+            />
+            <ImageListItemBar position="below" title={post.description} />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
     )
 }
+
+    
+        
+
+
 
 export default Photos
