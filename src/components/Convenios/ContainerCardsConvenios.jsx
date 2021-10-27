7d502971c3a38847d997f14d637fa02cf22b9faf
majoCerpe <mariajose_cerpe@outlook.com>
2021-10-27 11:00:45 -0300

    minor changes

--- src/components/Convenios/ContainerCardsConvenios.jsx
+++ src/components/Convenios/ContainerCardsConvenios.jsx
@@ -2,30 +2,35 @@ import { Grid } from "@mui/material";
 import React, { Fragment } from "react";
 import CardConvenios from "./CardConvenios";
 import convenios from "./convenios.json";
+import Box from '@mui/material/Box';
 
 
 
 const ContainerCards = () => {
-  
-  return (
-    <Fragment>
-      <h1>Convenios Corporativos</h1>
-      <Grid container spacing={1} justify="center">
-        {convenios.map((item) => {
-          return (
-            <CardConvenios
-              title={item.title}
-              description={item.description}
-              image={item.image}
-              text={item.text}
-              secondaryText={item.secondaryText}
-            />
-          );
-        })}
-      </Grid>
-    </Fragment>
-  );
-  }
+
+	return (
+		<Fragment>
+			<h2>Convenios Corporativos</h2>
+			<Grid container spacing={1} justify="center">
+				{convenios.map((item) => {
+					return (
+						<Box
+							sx={{ mt: 2, mb: 2, mx: 3 }} display="flex" justifyContent="center"
+						>
+							<CardConvenios
+								title={item.title}
+								description={item.description}
+								image={item.image}
+								text={item.text}
+								secondaryText={item.secondaryText}
+							/>
+						</Box>
+					);
+				})}
+			</Grid>
+		</Fragment>
+	);
+}
 
 
 export default ContainerCards;
